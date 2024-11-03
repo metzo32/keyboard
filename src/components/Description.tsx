630703b4afe6d2604d5dc4d2cb18eabc3d090359
METZ <metzo3232@gmail.com>
2024-11-03 17:56:48 +0900

    모달 추가

--- src/components/Description.tsx
+++ src/components/Description.tsx
@@ -17,16 +17,16 @@ export default function Description({ onClose }: DescriptionProps) {
         <IoClose className="icons"/>
       </button>
 
-        <ul>
-          <li>
+        <ul className="info-ul">
+          <li className="info-li">
             사용 중인 키보드의 배열에 따라 다르게 동작하는 것처럼 보일 수
             있습니다.
           </li>
-          <li>
-            브라우저 안정성을 위해 타이핑 모드에서 ESC 및 Function 키는 지원하지
+          <li className="info-li">
+            브라우저 정책에 따라 타이핑 모드에서 ESC 와 Function 키는 지원하지
             않습니다.
           </li>
-          <li>
+          {/* <li>
             <a
               href="https://smartstore.naver.com/dfshop1/products/10311254657?NaPm=ct%3Dm30wxxdy%7Cci%3Dcheckout%7Ctr%3Drete%7Ctrx%3Dnull%7Chk%3Dcdd72202dec441c8eb2f76b854f5c95026826df8"
               target="_blank"
@@ -34,7 +34,7 @@ export default function Description({ onClose }: DescriptionProps) {
             >
               구매 링크
             </a>
-          </li>
+          </li> */}
         </ul>
       </div>
     </div>
